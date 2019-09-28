4ace9003490d7091977ea12489512df45f6918a2
realityexpander <realityexpander@gmail.com>
2019-09-27 23:26:17 -0500

    Initial commit

---
+++ src/main/java/foo/Foo.java
@@ -0,0 +1,14 @@
+package foo;
+
+/**
+ * Created by Chris Athanas on 2019-08-07.
+ *
+ * https://android.jlelse.eu/how-to-use-android-studio-to-write-pure-java-23cbe49186e8
+ *
+ */
+public class Foo {
+  public static void main(String[] arg) {
+    String hi = "Hello World";
+    System.out.println(hi);
+  }
+}
